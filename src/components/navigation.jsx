fa6995241100587154e326851e3446799bebe94d
connor-kfitz <connorkfitzsimmons@gmail.com>
2022-10-23 18:43:38 -0400

    Added First Homepage Elements

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -5,19 +5,21 @@ export default function Navigation() {
 
     return (
         <nav>
-            <div class="nav-logo-cont">
-                <a href="/">Logo</a>
-            </div>
-            <div class="nav-links-cont">
-                <ul>
-                    <li>League of Legends</li>
-                    <li>Team Fight Tactics</li>
-                </ul>
-            </div>
-            <div class="nav-profile-info">
-                <div>Monkae</div>
-                <div class="nav-profile-img-cont">
-                    {/* <img src="https://via.placeholder.com/50"></img> */}
+            <div class="nav-container">
+                <div class="nav-logo-cont">
+                    <a href="/">Logo</a>
+                </div>
+                <div class="nav-links-cont">
+                    <ul>
+                        <li>League of Legends</li>
+                        <li>Team Fight Tactics</li>
+                    </ul>
+                </div>
+                <div class="nav-profile-info">
+                    <div>Monkae</div>
+                    <div class="nav-profile-img-cont">
+                        {/* <img src="https://via.placeholder.com/50"></img> */}
+                    </div>
                 </div>
             </div>
         </nav>
--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -5,6 +5,40 @@ export default function Homepage() {
 
     return (
         <main>
+            <div class="homepage-cont">
+                <section class="home-sec-one">
+                    <div>
+                        <h1>Hello, Monkae!</h1>
+                        <p>Some text about something</p> 
+                    </div> 
+                </section>
+                <section class="home-sec-two">
+                    <div class="history-slider">
+                        <ul>
+                            <li class="history-slider-on">Ranked Solo</li>
+                            <li>Ranked Flex</li>                     
+                        </ul>
+                    </div>
+                    <div class="twenty-game-history">
+                        <h1>20 Game History</h1>
+                        <div>Wins: 0 Loses: 0</div>
+                        <div class="twenty-game-kda-cont">
+                            <div class="twenty-game-stat">
+                                <div>10/10/10</div>
+                                <div>KDA</div>
+                                <div>P/Kill 62%</div>
+                            </div>
+                            <div class="twenty-game-graph">
+                               <div>Chart will go here</div> 
+                            </div>
+                        </div>
+                    </div>
+                    <div class="box-two">
+                        Box Two
+                    </div>
+                </section>
+
+            </div>
         </main>
     );
 }
