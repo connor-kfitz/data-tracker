08cc41daee71c77630aa2da7fe0a6972ff661347
connor-kfitz <connorkfitzsimmons@gmail.com>
2022-10-25 15:59:02 -0400

    Added Match History Skeleton HTML

---
+++ src/components/single-game-history.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "../styles/single-game-history-style.css";
+
+export default function SingleGameHistory() {
+
+    return (
+        <div>
+
+        </div>
+    );
+}
--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -1,19 +1,56 @@
-import React, { useState } from "react";
-import Axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 import "../styles/homepage-style.css";
 
-const apiKey = "RGAPI-9c2f6b68-2a4d-4f1d-8889-bb3d0b7126ed"
-
-async function fetchName(name){
-    Axios.get(`https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${name}?api_key=${apiKey}`).then((response) => {
-        console.log(response)
-    })
-
-}
+const apiKey = "RGAPI-9c2f6b68-2a4d-4f1d-8889-bb3d0b7126ed";
+var name = 'Monkæ'
+var puuid = "FE-AkSKHlt5HekAajyo3q0vgmLtH-64IKMcNk50aWE0XInp4AUVwAfWHSqQZKB3DFOeszLf7O8ReNQ";
+var matchId = "NA1_4473673180";
 
 export default function Homepage() {
 
+    const getPUUID = () => {
+        axios.get(`https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${name}?api_key=${apiKey}`)
+            .then(response => {
+                setPUUID(response.data.puuid)
+            }).catch(err => {
+                console.log(err)
+            })
+    }
+
+    const getMatchIds = () => {
+        axios.get(`https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/${PUUID}/ids?start=0&count=5&api_key=${apiKey}`)
+        .then((response) => {
+            setMatchIds(response.data)
+        }).catch(err => {
+            console.log(err)
+        })
+    }
+
+    const getMatchInfo = (id) => {
+            axios.get(`https://americas.api.riotgames.com/lol/match/v5/matches/${id}?api_key=${apiKey}`)
+            .then((response) => {
+                setMatchData(current => [...current, response])
+                console.log('g')
+                console.log(matchData)
+            }).catch(err => {
+                console.log(err)
+            })
+
+    }
+
+
     const [gameHistoryModeSelect, setGameHistoryModeSelect] = useState ('RankedSolo');
+    const [PUUID, setPUUID] = useState('');
+    const [matchIds, setMatchIds] = useState([]);
+    const [matchData, setMatchData] = useState([]);
+
+    
+    useEffect(() => {
+        getPUUID()
+        getMatchIds()
+    }, [])
+
 
     function setRankedSolo() {
         setGameHistoryModeSelect('RankedSolo')
@@ -23,7 +60,6 @@ export default function Homepage() {
         setGameHistoryModeSelect('RankedFlex')
     }
 
-    fetchName('Yolah');
 
     return (
         <main>
@@ -34,7 +70,129 @@ export default function Homepage() {
                         <p>Some text about something</p> 
                     </div>
                     <div className="match-history">
-                        <div>Test</div>
+                        <div>
+                            <h1>Flex 5:5 Rank</h1>
+                            <div>Defeat</div>
+                            <div>20m 33s</div>
+                        </div>
+                        <div>
+                            <div>
+                                <img></img>
+                            </div>
+                            <div>
+                                <div>
+                                    <img></img>
+                                </div>
+                                <div>
+                                    <img></img>
+                                </div>
+                                <div>
+                                    <img></img>
+                                </div>
+                                <div>
+                                    <img></img>
+                                </div>
+                            </div>
+                            <div>
+                                <div>0/4/0</div>
+                                <div>0:00:1 KDA</div>
+                            </div>
+                            <div>
+                                <div>
+                                    <img></img>
+                                </div>
+                                <div>
+                                    <img></img>
+                                </div>
+                                <div>
+                                    <img></img>
+                                </div>
+                                <div>
+                                    <img></img>
+                                </div>
+                                <div>
+                                    <img></img>
+                                </div>
+                                <div>
+                                    <img></img>
+                                </div>
+                                <div>
+                                    <img></img>
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <div>P/Kill 0%</div>
+                            <div>Control Ward 0</div>
+                            <div>CS 134 (6.5)</div>
+                            <div>Diamond 3</div>
+                        </div>
+                        <div>
+                            <div>
+                                <div>
+                                    <div>
+                                        <img></img>
+                                    </div>
+                                    <div>Name</div>
+                                </div>
+                                <div>
+                                    <div>
+                                        <img></img>
+                                    </div>
+                                    <div>Name</div>
+                                </div>
+                                <div>
+                                    <div>
+                                        <img></img>
+                                    </div>
+                                    <div>Name</div>
+                                </div>
+                                <div>
+                                    <div>
+                                        <img></img>
+                                    </div>
+                                    <div>Name</div>
+                                </div>
+                                <div>
+                                    <div>
+                                        <img></img>
+                                    </div>
+                                    <div>Name</div>
+                                </div>
+                            </div>
+                            <div>
+                                <div>
+                                    <div>
+                                        <img></img>
+                                    </div>
+                                    <div>Name</div>
+                                </div>
+                                <div>
+                                    <div>
+                                        <img></img>
+                                    </div>
+                                    <div>Name</div>
+                                </div>
+                                <div>
+                                    <div>
+                                        <img></img>
+                                    </div>
+                                    <div>Name</div>
+                                </div>
+                                <div>
+                                    <div>
+                                        <img></img>
+                                    </div>
+                                    <div>Name</div>
+                                </div>
+                                <div>
+                                    <div>
+                                        <img></img>
+                                    </div>
+                                    <div>Name</div>
+                                </div>
+                            </div>
+                        </div>
                     </div> 
                 </section>
                 <section className="home-sec-two">
