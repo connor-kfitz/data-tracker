09c137339b16ab678b67008360fe68180a2ed1ed
connor-kfitz <connorkfitzsimmons@gmail.com>
2022-10-26 12:07:46 -0400

    Added Map Info & Game Type Info

--- src/components/single-game-history.jsx
+++ src/components/single-game-history.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 import "../styles/single-game-history-style.css";
 
-export default function SingleGameHistory(gameData) {
+const apiKey = "RGAPI-08d7d2f3-dcbc-4884-ac36-e9f4cef5f79b";
+
+
+export default function SingleGameHistory(props) {
+
+    const [matchData, setMatchData] = useState([]);
+    const [mapData, setMapData] = useState([]);
+    const [gameTypeData, setGameTypeData] = useState([]);
+
+    const getGameData = async() => {
+
+        const getMatchData = await (axios.get(`https://americas.api.riotgames.com/lol/match/v5/matches/${props.gameData}?api_key=${apiKey}`));
+        setMatchData(getMatchData);
+
+        const getMapInfo = await (axios.get(`https://static.developer.riotgames.com/docs/lol/maps.json`));
+        setMapData(getMapInfo);
+
+        const getGameTypeData = await (axios.get(`https://static.developer.riotgames.com/docs/lol/queues.json`));
+        setGameTypeData(getGameTypeData);
+
+    }
+
+    useEffect(() => {
+        getGameData();
+    },[])
+
+    useEffect(() => {
+        console.log(`This is the match data`);
+        console.log()
+    },[matchData])
 
     return (
         <div className="history-container">
